8f0bc411c32413f373035c65f6cd8fea2d2845f6
Michael Fuget <michaelfuget@gmail.com>
2020-12-06 07:42:25 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function largestSubarraySum(array){
+ // code to write here
+ const running_sum = Array(array.length).fill(0);
+ var currentMax = 0;
+ var runningSum[0] = array[0];
+ for(var i=1; i<array.lenth; i++)
+ {
+    var newSum = runningSum[i-1] + array[i]
+    if(newSum > array[i])
+      runningSum[i] = newSum
+    else
+      runningSum[i] = array[i]
+    end 
+    if(runningSum[i] > currentMax)
+    {      
+      currentMax = runningSum[i]
+    }
+  }
+  return currentMax
+ 
+}
